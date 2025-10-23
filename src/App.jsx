3cb938e7a55d86f5e3e0ead3c2c29407f6926c0e
Bazif Abdullah <bazifabdullah@gmail.com>
2025-10-24 01:03:54 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-white">
+      <h1 className="text-2xl font-bold text-center pt-10">
+        REST Countries API
+      </h1>
+    </div>
+  );
+}
+
+export default App;
