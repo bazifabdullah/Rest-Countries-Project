763a94a2cc904ac7b1e47b56fba4eecd53be4adb
Bazif Abdullah <bazifabdullah@gmail.com>
2025-10-31 22:41:44 +0500

    Add search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ function App() {
   const [countries, setCountries] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [search, setSearch] = useState("")
 
   useEffect(() => {
     const fetchCountries = async () => {
       try {
         const response = await axios.get("https://restcountries.com/v3.1/all?fields=name,flags,region,capital,population");
 
+        //Filtering countries that are not real 
         const filtered = response.data.filter(
           (country) =>
           country.name.common !== "Israel"
@@ -32,21 +34,28 @@ function App() {
     fetchCountries();
   }, [])
 
+  //Function to show the searched countries
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white">
       <Header />
       <main className="px-6 py-10 max-w-7xl mx-auto">
         <div className="flex flex-col sm:flex-row justify-between gap-6 mb-10">
-          <SearchBar />
+          <SearchBar search={search} setSearch={setSearch}/>
           <RegionFilter />
         </div>
         
         {loading ? (
           <p className="text-center">Loading Countries...</p>
         ) : error ? (
-            <p className="text-center text-red-500">{error}</p>
-          ) : (
-            <CountryGrid countries={countries} />
+            <p className="text-center text-red-500 font-semibold">{error}</p>
+        ) : filteredCountries.length === 0 ? (
+            <p className="text-center text-red-500 font-semibold">No Country Found. 404</p>
+        ) : (
+            <CountryGrid countries={filteredCountries} />
         )
         }
       </main>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { Search } from "lucide-react";
 
-const SearchBar = () => {
+const SearchBar = ({search, setSearch}) => {
     return (
         <div className="flex items-center bg-white dark:bg-gray-800 shadow-md rounded-md px-4 py-3 w-full sm:w-80">
             <Search className="w-5 h-5 text-gray-500 dark:text-gray-300 mr-3" />
@@ -8,6 +8,8 @@ const SearchBar = () => {
                 type="text"
                 placeholder="Search for a country..."
                 className="w-full bg-transparent outline-none text-gray-900 dark:text-white placeholder-gray-500 dark:placeholder-gray-400"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
             />
         </div>
     )
