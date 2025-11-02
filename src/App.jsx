223d7c1b57c22bf9af4de4cd064d9d7f33b503be
Bazif Abdullah <bazifabdullah@gmail.com>
2025-11-02 13:02:11 +0500

    Add routing structure in the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState,useEffect } from "react";
 import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import RegionFilter from "./components/RegionFilter";
@@ -46,21 +48,30 @@ function App() {
     <div className="min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white">
       <Header />
       <main className="px-6 py-10 max-w-7xl mx-auto">
-        <div className="flex flex-col sm:flex-row justify-between gap-6 mb-10">
-          <SearchBar search={search} setSearch={setSearch}/>
-          <RegionFilter selectedRegion={selectedRegion} setSelectedRegion={setSelectedRegion}/>
-        </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="flex flex-col sm:flex-row justify-between gap-6 mb-10">
+                  <SearchBar search={search} setSearch={setSearch}/>
+                  <RegionFilter selectedRegion={selectedRegion} setSelectedRegion={setSelectedRegion}/>
+                </div>
         
-        {loading ? (
-          <p className="text-center">Loading Countries...</p>
-        ) : error ? (
-            <p className="text-center text-red-500 font-semibold">{error}</p>
-        ) : filteredCountries.length === 0 ? (
-            <p className="text-center text-red-500 font-semibold">No Country Found. 404</p>
-        ) : (
-            <CountryGrid countries={filteredCountries} />
-        )
-        }
+                {loading ? (
+                    <p className="text-center">Loading Countries...</p>
+                  ) : error ? (
+                      <p className="text-center text-red-500 font-semibold">{error}</p>
+                  ) : filteredCountries.length === 0 ? (
+                      <p className="text-center text-red-500 font-semibold">No Country Found. 404</p>
+                  ) : (
+                      <CountryGrid countries={filteredCountries} />
+                  )
+                }
+              </>
+            }
+          />
+        </Routes>
       </main>
     </div>
   );
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,29 +1,33 @@
+import { Link } from "react-router-dom";
+
 const CountryCard = ({ country }) => {
     return (
-        <div className="bg-white dark:bg-gray-800 rounded-md overflow-hidden hover:scale-[1.02] transition-transform duration-200 cursor-pointer">
-            <img
-                src={country.flags?.png}
-                alt={country.flags?.alt || `${country.name.common} flag`}
-                className="w-full h-48 sm:h-40 object-cover"
-            />
-            <div className="p-6">
-                <h2 className="font-bold text-lg mb-3">
-                    {country.name.common}
-                </h2>
-                <p className="text-sm">
-                    <span className="font-semibold">Population:   </span>
-                    {country.population.toLocaleString()}
-                </p>
-                <p className="text-sm">
-                    <span className="font-semibold">Region:   </span>
-                    {country.region}
-                </p>
-                <p className="text-sm">
-                    <span className="font-semibold">Capital:   </span>
-                    {country.capital?.[0] || "N/A"}
-                </p>
+        <Link to={`/country/${country.name.common}`}>
+            <div className="bg-white dark:bg-gray-800 rounded-md overflow-hidden hover:scale-[1.02] transition-transform duration-200 cursor-pointer">
+                <img
+                    src={country.flags?.png}
+                    alt={country.flags?.alt || `${country.name.common} flag`}
+                    className="w-full h-48 sm:h-40 object-cover"
+                />
+                <div className="p-6">
+                    <h2 className="font-bold text-lg mb-3">
+                        {country.name.common}
+                    </h2>
+                    <p className="text-sm">
+                        <span className="font-semibold">Population:   </span>
+                        {country.population.toLocaleString()}
+                    </p>
+                    <p className="text-sm">
+                        <span className="font-semibold">Region:   </span>
+                        {country.region}
+                    </p>
+                    <p className="text-sm">
+                        <span className="font-semibold">Capital:   </span>
+                        {country.capital?.[0] || "N/A"}
+                    </p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
